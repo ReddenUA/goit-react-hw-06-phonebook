c94a417381ec32ca6849666ae9a46993bed70549
ReddenUA <96418744+ReddenUA@users.noreply.github.com>
2022-12-16 18:16:29 +0100

    Update ContactForm.styled.jsx

--- src/components/ContactForm/ContactForm.styled.jsx
+++ src/components/ContactForm/ContactForm.styled.jsx
@@ -18,7 +18,6 @@ export const BtnAdd = styled.button`
   margin-top: 12px;
   border-radius: 5px;
   cursor: pointer;
-  border:none;
  box-shadow: 0px 2px 1px -1px rgb(0 0 0 / 20%),
     0px 1px 1px 0px rgb(0 0 0 / 14%), 0px 1px 3px 0px rgb(0 0 0 / 12%);
   transition: background-color 250ms linear;
